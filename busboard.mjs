b99ccac0b9156fe5e168cf111a63eb7a0e1f4b70
Patricia Robles <patriciarobles9@gmail.com>
2022-05-03 12:24:34 +0100

    simple (non-function) version of busboard

--- busboard.mjs
+++ busboard.mjs
@@ -4,14 +4,16 @@ import readLine from "readline";
 const stopID = "490008660N";
 
 async function getBusInfoFor(stopID) {
-  const url = `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`;
+  const url = `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals?app_key=5716904db8c14735b9a633fd6523ee11`;
   const response = await fetch(url);
   const busInfo = await response.json();
 
-  return busInfo;
+  return await busInfo;
 }
 
 function logBusArrivalTimes(busInfo) {
+  const arrivals = busInfo;
+
   arrivals.sort((a, b) => a.timeToStation - b.timeToStation);
 
   arrivals.forEach((busArrival) => {
@@ -30,7 +32,7 @@ function logBusArrivalTimes(busInfo) {
 async function getPostcodeData(postcode) {
   const urlForPostCodeRequest = `https://api.postcodes.io/postcodes/${postcode}`;
   const postcodeJSON = await fetch(urlForPostCodeRequest);
-  return postcodeJSON.json();
+  return await postcodeJSON.json();
 }
 
 async function getCloseStopIDS(coords) {
@@ -39,7 +41,24 @@ async function getCloseStopIDS(coords) {
   const stopsIdAPI = await fetch(
     `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanPublicBusCoachTram&radius=500`
   );
-  return stopsIdAPI.json();
+  return await stopsIdAPI.json();
+}
+
+function sortBusStopData(postcodeJSON) {
+  const stopIDs = postcodeJSON.stopPoints;
+  const busStopInfo = [];
+
+  stopIDs.forEach((stop) => {
+    busStopInfo.push({
+      id: stop.naptanId,
+      distance: Math.floor(stop.distance),
+    });
+  });
+
+  busStopInfo.sort((a, b) => {
+    return a.distance - b.distance;
+  });
+  return busStopInfo.slice(0, 2);
 }
 
 const rl = readLine.createInterface({
@@ -50,26 +69,55 @@ const rl = readLine.createInterface({
 rl.question("What is your postcode? ", async (answer) => {
   //TODO: potentially do some error handling here
   const postcode = answer.toLocaleUpperCase().trim();
-  const postcodeData = await getPostcodeData(postcode);
+  let postcodeData;
 
+  let postcodeValid = false;
+  while (!postcodeValid) {
+    
+    try {
+        postcodeData = await getPostcodeData(postcode);
+        if (!postcodeValidationJSON.result) { //.result is object value ~ si no existe el objeto creado cdo se da un postcode valido a la api, throw err
+            throw new Error ('Invalid postcode entered')
+        }
+        else {postcodeValid = true;}
+
+    } catch (err) {
+        console.log ('Postcode not recognised. \nPlease enter a valid postcode.')
+        userPostcode = readline.prompt();    
+    }
+}
+  //if postcode data is good, continue on.
+  //Else prompt use to try again.
+  //if it still doesn't work quit with letting user know the reason
   const postcodeLocation = {
     longitude: postcodeData.result.longitude,
     latitude: postcodeData.result.latitude,
   };
 
-  const stopIDS = await getCloseStopIDS(postcodeLocation);
+  const stopIDsJSON = await getCloseStopIDS(postcodeLocation);
+  //if tfl data is good, continue on.
+  //TODO: if not good, do what?
+  const stopIDs = sortBusStopData(stopIDsJSON);
 
-  console.log(stopIDS);
+  //TODO: do we need this for the user?
+  console.log(
+    `\nLogging data for these stop ids: ${stopIDs[0].id}, ${stopIDs[1].id}`
+  );
+
+  for (const stop of stopIDs) {
+    const busInfo = await getBusInfoFor(stop.id);
+    //if tfl data is good, continue on.
+    //TODO: if not good, do what?
+    console.log(`\n\nBus times for stop: ${busInfo[0].stationName}\n`);
+    logBusArrivalTimes(busInfo);
+  }
+
   rl.close();
 });
 
-//get users input for postcode
-//remove any spaces + captilise it
-//get long and lat
+//TODO: actually log out bus times ✅
+//TODO: validate the postcode
+//TODO: validate the bus stops - what if it returns an empty array
 
-//url = api.postcodes.io/postcodes/{postcode}
-//validate => if status 404 then inform user
-//retrieve longitude + latitude
-//coords for testing = lat = 51.55411 long = -0.292968
-// url for bus stops within long + lat = https://api.tfl.gov.uk/StopPoint/?lat={lat}&lon={lon}&stopTypes={stopTypes}[&radius]
-//stop typpes = look for NaptanId watch for children (stop on either side of the road)
+//TODO: Fix crashing if theres no bus stops
+//TODO: winston library
--- busboardsimple.mjs
+++ busboardsimple.mjs
@@ -1,36 +1,82 @@
-import fetch from "node-fetch";
+import fetch from "node-fetch"; 
+import readline from "readline-sync"; 
 
-const postcode = 'W1A 1AA';
-const postcodeAPI = await fetch (`https://api.postcodes.io/postcodes/${postcode}`);
-const postcodeData = await postcodeAPI.json();
+//get user postcode input & verification
+console.log('Please enter a postcode.');
+let userPostcode = readline.prompt();
 
-const postcodeLongitude = postcodeData.result.longitude;
-const postcodeLatitude = postcodeData.result.latitude;
+//verify user input 
+let postcodeValid = false; // to force code down the while loop
 
-const stopsAPI = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${postcodeLatitude}&lon=${postcodeLongitude}&stopTypes=NaptanPublicBusCoachTram`)
-const stops = await stopsAPI.json();
-console.log(stops)
+while (!postcodeValid) {
+    
+    try {
+        const postcodeValidationAPI = await fetch(`https://api.postcodes.io/postcodes/${userPostcode}`);
+        const postcodeValidationJSON = await postcodeValidationAPI.json();
+        if (!postcodeValidationJSON.result) { //.result is object value ~ si no existe el objeto creado cdo se da un postcode valido a la api, throw err
+            throw new Error ('Invalid postcode entered')
+        }
+        else {postcodeValid = true;}
 
-stops.stopPoints.forEach(stopPoint => {
-    console.log(`Stop called ${stopPoint.commonName}is located ${Math.floor(stopPoint.distance)} metres away.`)
-})
+    } catch (err) {
+        console.log ('Postcode not recognised. \nPlease enter a valid postcode.')
+        userPostcode = readline.prompt();    
+    }
+}
 
-const stopID = "490008660N";
-const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`);
-const arrivals = await response.json();
+//longitude and latitude data
+const postcodeAPI = await fetch(`https://api.postcodes.io/postcodes/${userPostcode}`);
+const postcodeJSON = await postcodeAPI.json();
+const longitude = postcodeJSON.result.longitude;
+const latitude = postcodeJSON.result.latitude;
 
-//const sortedArrivals = arrivals.sort((a, b) => a.timeToStation - b.timeToStation);
 
-arrivals.forEach((busArrival) => {
-    const minutesUntilBusArrives = Math.floor(busArrival.timeToStation / 60);
-    console.log(`Bus to ${busArrival.destinationName} arriving in ${minutesUntilBusArrives} minutes.`);
- });
+//TfL API - closest TWO bus stops
+
+// try
+const busStopsAPI = await fetch (`https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=NaptanPublicBusCoachTram&radius=200`)
+const busStopsJSON = await busStopsAPI.json();
+//if (busStopsJSON === {}) {
+    //throw 'No bus stops within set radius'
+//} else {
+
+const localBusStops = busStopsJSON.stopPoints;
+const closestBusStops = [];
+localBusStops.forEach(stop => closestBusStops.push([stop.commonName, Math.round(stop.distance), stop.id]));
+const key2BusStops = closestBusStops.sort((a,b) => a.distance - b.distance).slice(0,2)
+//}
+// catchexeption is no bus stops in vecinity
+//catch (err) {
+// console.log('There are no bus tops nearby / specified ratius)
+// throw err
+//}
+
+
+
+
+// Loop to get ID of closest TWO stops to confirm arrivals
+let stopID = ''
+for(let i = 0; i < 2; i++) {
+    stopID = key2BusStops[i][2]
+}
+const arrivalsAPI = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`)
+const arrivalsJSON = await arrivalsAPI.json();
+console.log(arrivalsJSON)
+
+const nextArrivals = []
+arrivalsJSON.forEach(bus => nextArrivals.push([bus.lineName, bus.destinationName, Math.round(bus.timeToStation/60)]));
+const sortedArrivals = nextArrivals.sort((a,b)=> a.timeToStation - b.timeToStation) // not sorting???
+//console.log(sortedArrivals)
+for (let i = 0; i < sortedArrivals.length; i++) {
+        console.log(`Bus ${sortedArrivals[i][0]} towards ${sortedArrivals[i][1]} will arrive in ${sortedArrivals[i][2]} minutes`);
+}
  
+//CONSIDER...
+//differenciate between incorrect data entered and correct postcode outside London region
+//error handling for no stops in set radius
+//error handling for no buses coming
 
- //TO DOs
- // get postcode as input from individual
- // allow individual to see next two buses according to postcode
- // think about postcode validation (do... while loop? keep asking for input if invalid data entered)
-//  no bus stop nearby? ... wouldnt the API return the closest? offer closest alternative ?
- // ask input re directions required to arrive to bus stop
+//CHECK..
+//is sorting of next two buses according to time actually happening?? sorting bus number??
+
 
